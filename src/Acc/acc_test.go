10a65a93060f6bb07460cd996f4fbf4ec321b820
GarryFCR <garryranee@gmail.com>
2021-10-03 20:03:13 +0530

    Tests for update

--- src/Acc/acc_test.go
+++ src/Acc/acc_test.go
@@ -2,7 +2,6 @@ package Acc
 
 import (
 	"crypto/rand"
-	//"fmt"
 	"math/big"
 	"testing"
 )
@@ -47,6 +46,40 @@ func TestGenerate_acc(t *testing.T) {
 		if witnesses[i].Cmp(&witness) != 0 {
 			t.Fatalf("Incorrect witness")
 		}
+	}
+
+	//Update(Add)------------------------------------------------------------------------------
+	Accumulator_prev1 := Accumulator.Acc
+	Accumulator.Add_member(*big.NewInt(127), w)
+	e := Hprime(*big.NewInt(127))
+	Accumulator_prev1.Exp(&Accumulator_prev1, &e, &key.N)
+	if Accumulator_prev1.Cmp(&Accumulator.Acc) != 0 {
+		t.Fatalf("Incorrect value of Accumulator after update")
+	}
+
+	U = append(U[:], *big.NewInt(127))
+	for i, u := range U {
+		if u.Cmp(&Accumulator.U[i]) != 0 {
+			t.Fatalf("Incorrect update of set")
+		}
+	}
+	u := big.NewInt(127)
+	W1 := generate_witness(*big.NewInt(127), key, U)
+	W2 := w.List[u.String()]
+	if W1.Cmp(&W2) != 0 {
+		t.Fatalf("Incorrect witness computation after update")
+	}
+	//Update(Delete)------------------------------------------------------------------------------
+	Accumulator.Delete_member(*big.NewInt(126), w)
+	Accumulator_prev2 := Generate_Acc(key, Accumulator.U)
+	if Accumulator_prev2.Acc.Cmp(&Accumulator.Acc) != 0 {
+		t.Fatalf("Incorrect value of Accumulator after delete")
+	}
+
+	W3 := generate_witness(*big.NewInt(127), key, Accumulator.U)
+	W4 := w.List[u.String()]
+	if W4.Cmp(&W3) != 0 {
+		t.Fatalf("Incorrect witness computation after update")
 	}
 
 }
