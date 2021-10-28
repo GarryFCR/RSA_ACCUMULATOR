fff4645a3bc30fdcab7ecc756199b42b45c51e50
GarryFCR <garryranee@gmail.com>
2021-10-28 13:03:08 +0530

    run time check

--- simulation.go
+++ simulation.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math/big"
 	"math/rand"
 	"time"
@@ -13,33 +14,41 @@ import (
 )
 
 const (
-	loops       = 10
-	sliceLength = 1000
+	loops       = 5
+	sliceLength = 100
 )
 
 var (
 	source  = rand.NewSource(time.Now().UnixNano())
-	key     = Acc.Rsa_keygen(128) //group order should be greater than the elements
+	key     = Acc.Rsa_keygen(512) //group order should be greater than the elements
 	letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
 	random  = rand.New(source)
 )
 
 func main() {
 
-	a := big.NewInt(0).SetBytes(randomAddress().Bytes())
-	fmt.Println(a.Div(a, big.NewInt(9223372036854775807)).String())
-
+	fmt.Println("For set size:", sliceLength)
 	for loop := 0; loop < loops; loop++ {
 		array := make([]big.Int, sliceLength)
 		for i, _ := range array {
 			array[i] = *big.NewInt(0).SetBytes(randomAddress().Bytes())
 		}
+		start1 := time.Now()
 		Accumulator := Acc.Generate_Acc(key, array)
 		w := Accumulator.Witness_int()
+		elapsed1 := time.Since(start1)
+		log.Printf("Accumulator generation time :%s", elapsed1)
+
+		start2 := time.Now()
 		w.Precompute_witness(Accumulator.G, Accumulator.U, Accumulator)
+		elapsed2 := time.Since(start2)
+		log.Printf("witness computation time :%s", elapsed2)
+
 		t, fp, fn := check(array, w, Accumulator)
 		fmt.Println("loop:", loop, "t:", t, "fp:", fp, "fn:", fn)
+		fmt.Println()
 	}
+
 }
 
 func randomAddress() sdk.AccAddress {
